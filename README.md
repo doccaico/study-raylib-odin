879f4d6cfdcba2cd849abcb5aec612687f2c4358
doccaico <hash_ext_luna@yahoo.co.jp>
2025-06-07 09:28:55 +0900

    up

---
+++ README.md
@@ -0,0 +1,12 @@
+## study-raylib-odin
+
+### Build and Run
+```sh
+## Debug
+$ run.cmd lifegame
+## or
+$ run.cmd lifegame --debug
+
+## Release
+$ run.cmd lifegame --release
+```
---
+++ run.cmd
@@ -0,0 +1,47 @@
+@echo off
+
+
+setlocal
+
+
+:: Debug = 0, Release = 1
+if "%2" == "" (
+    set release_mode=0
+) else if "%2" == "--debug" (
+    set release_mode=0
+) else if "%2" == "--release" (
+    set release_mode=1
+) else (
+    goto :HELP
+)
+
+
+set flags=--keep-executable
+
+if %release_mode% equ 0 ( REM Debug
+    set flags=%flags% -debug
+) else ( REM Release
+    set flags=%flags% -o:speed
+)
+
+
+if        "%1" == "lifegame"        ( goto :LIFEGAME
+REM ) else if "%1" == "window"          ( goto :WINDOW
+) else                              ( goto :HELP
+)
+
+
+:HELP
+    echo Usage : $ run.cmd [directory's name] [--debug^|--release]
+    echo   Example: $ run.cmd lifegame           (debug mode)
+    echo            $ run.cmd lifegame --debug   (debug mode)
+    echo            $ run.cmd lifegame --release (release mode)
+goto :EOF
+
+
+:LIFEGAME
+    odin run %1 %flags% -out:%1\%1.exe
+goto :EOF
+
+
+REM vim: ft=dosbatch fenc=utf8 ff=dos
